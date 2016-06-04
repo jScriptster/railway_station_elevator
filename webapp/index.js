b925f7596cbada316514f5b10b2a119cb6e14a21
Mathias Kotowski <mathias.kotowski@gmail.com>
2016-06-04 21:06:12 +0200

    fetch elevator-data

--- webapp/index.js
+++ webapp/index.js
@@ -1,27 +1,37 @@
 var express = require('express'),
     stationService = require('./model/station-service.js')(),
-    elevatorService = require('./model/elevator-service.js')();
+    elevatorService = require('./model/elevator-service.js')(),
+    packageJSON = require('./package.json');
 
 var app = express();
+
+var urlStatic = '/static/' + packageJSON.version;
+var urlApiStationSearch = '/api/station/search/:query';
 
 app.set('views', __dirname + '/view');
 app.set('view engine', 'jade');
 
-app.get('/api/station/search/:query', (req, res) => {
+app.get(urlApiStationSearch, (req, res) => {
     res.json(stationService.search(req.params.query));
 });
 
-app.get('/api/elevator/search-by-station/:id', (req, res) => {
-    res.json(elevatorService.searchByStationId(req.params.id));
-});
-
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        numberCurrentlyInactiveElevators: elevatorService.numberCurrentlyInactive,
+        numberCurrentlyActiveElevators: elevatorService.numberCurrentlyActive,
+        url: {
+            elevators: elevatorService.urlElevators,
+            stations: elevatorService.urlStations,
+            static: urlStatic,
+            apiStationSearch: urlApiStationSearch
+        }
+    });
 });
 
-app.use('/static', express.static('public'));
+app.use(urlStatic, express.static('public'));
 
 app.listen(3000, () => {
     console.log('listening on port 3000...');
     stationService.fetch();
+    elevatorService.fetch();
 });
--- webapp/model/elevator-service.js
+++ webapp/model/elevator-service.js
@@ -1,18 +1,65 @@
+"use strict";
+
 const HOST = 'adam.noncd.db.de';
 const PATH_BASE = '/api/v1.0';
 const PATH_FACILITIES = '/facilities';
-const PATH_STATION = '/stations';
+const PATH_STATIONS = '/stations';
 
+var fetchOptions = {
+    host: HOST,
+    path: PATH_BASE + PATH_FACILITIES,
+    headers: {
+        "user-agent": "jScriptster/railway_station_elevator Node.js", // strangely, the api requires a user agent
+        "accept": "application/json" // accept header is required
+    }
+};
 
+var https = require('https');
 
 var singletonServiceInstance = {
-    searchByStationId (id) {
-        return {
-            q: id,
-            payload: {
 
-            }
-        };
+    facilitiesFull: [],
+
+    currentlyActive: 0,
+    currentlyInactive: 0,
+
+    fetch () {
+        return https.get(fetchOptions, (response) => {
+            // Continuously update stream with data
+            var body = '';
+            response.on('data', (d) => {
+                body += d;
+            });
+            response.on('end', () => {
+                // Data reception is done, do whatever with it!
+                this.currentlyActive = 0;
+                this.currentlyInactive = 0;
+                this.facilitiesFull = JSON.parse(body);
+                for (let i = 0, len = this.facilitiesFull.length; i < len; i++) {
+                    if (this.facilitiesFull[i].state === 'ACTIVE') {
+                        this.currentlyActive++;
+                    } else if (this.facilitiesFull[i].state === 'INACTIVE') {
+                        this.currentlyInactive++;
+                    }
+                }
+            });
+        });
+    },
+
+    get numberCurrentlyInactive () {
+        return this.currentlyInactive;
+    },
+
+    get numberCurrentlyActive () {
+        return this.currentlyActive;
+    },
+
+    get urlElevators () {
+        return 'https://' + HOST + PATH_BASE + PATH_FACILITIES;
+    },
+
+    get urlStations () {
+        return 'https://' + HOST + PATH_BASE + PATH_STATIONS;
     }
 };
 
