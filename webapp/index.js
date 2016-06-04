c6225f9d212b26942e4819d8d4c26c7706442dae
Mathias Kotowski <mathias.kotowski@gmail.com>
2016-06-04 16:51:41 +0200

    add station-service

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var stationService = require('./model/station-service.js')();
+
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/api/stationsearch/:query', (req, res) => {
+    res.json(stationService.search(req.params.query));
 });
 
-app.listen(3000, function () {
+app.use('/', express.static('public'));
+
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
+    stationService.fetch();
 });
---
+++ webapp/model/station-service.js
@@ -0,0 +1,87 @@
+"use strict";
+const DATA_URL = "http://127.0.0.1:3000/stations.csv";
+
+var headers = new Array(13).fill('unused');
+headers[0] = 'land';
+headers[2] = 'id';
+headers[3] = 'stationname';
+headers[8] = 'city';
+
+var CSVConverter = require("csvtojson").Converter;
+var csvConverter = new CSVConverter({
+    constructResult: false,
+    delimiter: ';',
+    flatKeys: true,
+    headers: headers
+});
+
+
+csvConverter.transform = function(json,row,index){
+     delete json["unused"];
+};
+
+var singletonServiceInstance = {
+
+    data: [],
+
+    fetch () {
+        var newData = [];
+
+        //record_parsed will be emitted each csv row being processed
+        csvConverter.on("record_parsed", (jsonObj) => {
+            newData.push(jsonObj);
+        });
+
+        csvConverter.on("end_parsed", (jsonObj) => {
+            this.data = newData;
+        });
+
+        require("request").get(DATA_URL).pipe(csvConverter);
+    },
+
+    search (query) {
+        var result = {
+            q: query.trim(),
+            payload: {
+                entries: []
+            }
+        };
+
+        var queryArr = result.q.split(' ');
+
+        result.payload.entries = this.data.filter((elem, index, arr) => {
+            var hitCount = 0,
+                queryPart;
+            if (queryArr.length === 1) {
+                queryPart = queryArr[0];
+                if (queryPart.length < 6) {
+                    hitCount += (elem.stationname.toLowerCase().indexOf(queryPart) === 0
+                        || elem.city.toLowerCase().indexOf(queryPart) === 0) ? 1 : 0;
+                } else {
+                    hitCount += (elem.stationname.toLowerCase().indexOf(queryPart) > -1
+                        || elem.city.toLowerCase().indexOf(queryPart) > -1) ? 1 : 0;
+                }
+
+            } else {
+                for (let i = 0, len = queryArr.length; i < len; i++) {
+                    queryPart = queryArr[i];
+                    if (queryPart.length < 2) {
+                        hitCount += (elem.stationname.toLowerCase().indexOf(queryPart) === 0
+                            || elem.city.toLowerCase().indexOf(queryPart) === 0) ? 1 : 0;
+                    } else {
+                        hitCount += (elem.stationname.toLowerCase().indexOf(queryPart) > -1
+                            || elem.city.toLowerCase().indexOf(queryPart) > -1) ? 1 : 0;
+                    }
+                }
+            }
+
+            return hitCount === queryArr.length;
+        });
+
+        return result;
+    }
+};
+
+module.exports = function () {
+    return singletonServiceInstance;
+};
